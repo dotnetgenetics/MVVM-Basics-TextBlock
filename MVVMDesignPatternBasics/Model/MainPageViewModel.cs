91d3dad2fa17560522bd852edd5f9f8c2834ae26
dotnetgenetics <gregory_keyboard[@]outlook.com>
2017-07-10 13:21:22 +0800

    First Check-in MVVM Basic using TextBlock

---
+++ MVVMDesignPatternBasics/Model/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMDesignPatternBasics.Model
+{
+    public class MainPageViewModel : BindableBase<Book>
+    {
+        private Book _book;
+
+        public Book Book
+        { 
+            get
+            {
+                return _book;
+            } 
+            set
+            {
+                SetProperty(ref _book, value);
+            } 
+        }
+        
+        public MainPageViewModel()
+        {
+            Book = new Book()
+            {
+                Title = "Harry Potter",
+                Author = "J. K. Rowling",
+                Category = "Young-adult fiction",
+                Language = "English"
+            };
+        }
+    }
+}
